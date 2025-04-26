850037a42965bb4c92ad3756d239f117dc47a74d
Zaheer Sani <sani.zaheer@gmail.com>
2025-04-26 13:03:04 +0500

    TODO: JSONPlaceHolder API TODOs + Bootstrap + Nav Tabs + Modal + Task

--- jQueryAPI/jsonplaceholder.js
+++ jQueryAPI/jsonplaceholder.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
-
+    // TODO: Add functionality for Users and Posts, similar to Todos
+    
     getTodos();
     getUsers();
 
     function getTodos() {
+        // Fetch data from the JSONPlaceholder API for todos
         $.get('https://jsonplaceholder.typicode.com/todos', function(data) {
-            console.log(data);
+            // Remove existing list items before appending new ones
+            $('#todo-list').empty();
+            // Set todos variable to the fetched data
+            var todos = data;
+            // Loop through the data and append each todo to the list
+            for (var i = 0; i < data.length; i++) {
+                var todo = data[i];
+                // Append each todo to the list with a unique id
+                $('#todo-list').append('<li class="list-group-item" data-bs-toggle="modal" data-bs-target="#exampleModal" id="' + i + '">' + todo.title + '</li>');
+            }
+
+            // add click event to each li element after appending
+            $('#todo-list li').click(function() {
+                // fetch id attribute from the clicked li element
+                var id = $(this).attr('id');
+                // set title and body of the modal
+                $(".modal-title").text("Todo ID: " + (parseInt(id)+1));
+                $(".modal-body").text(todos[id].title);
+            });
         });
     }
 
