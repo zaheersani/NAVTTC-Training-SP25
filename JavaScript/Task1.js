8b7e687b3e819940e25fd7b4c9bb7ce37d38b6bb
Zaheer Sani <sani.zaheer@gmail.com>
2025-04-27 11:01:57 +0500

    JavaScript Task1

---
+++ JavaScript/Task1.js
@@ -0,0 +1,50 @@
+// Task 1: Declare Variables
+// Declare three variables using let, const, and var.
+// Assign them any values.
+// Print their values using console.log()
+
+
+
+// Task 2: Identify Data Types
+// Create variables with values of the following types:
+// - A number
+// - A string
+// - A boolean
+// - undefined
+// - null
+// - An object
+// Print the data type of each variable using typeof
+
+
+
+// Task 3: Reassign Variables
+// Declare a variable with let.
+// Assign it an initial value.
+// Reassign it to a new value.
+// Try doing the same with a const variable and observe what happens.
+
+
+// Task 4: Practice undefined and null
+// Create a variable but do not assign any value.
+// Later, assign it the value null.
+// Print its value after each step.
+
+
+
+// Task 5: Create Different Objects
+// Create an object that represents a car.
+// It should have properties like brand, model, and year.
+// Print the object and the type of object.
+
+
+
+// Task 6: Boolean Values
+// Declare two boolean variables:
+// - One representing "Is the user logged in?"
+// - One representing "Is the user an admin?"
+// Print both variables.
+
+
+// Task 7: Bonus Challenge
+// Guess the output before running:
+
